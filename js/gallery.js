068de950ede085551b6e6562692b21d47ee0aaf9
gogethubby <leogilreath@comcast.net>
2019-05-07 17:08:35 -0700

    changes

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,9 @@
 TweenMax.to(".woids", 4, {x:20, ease:Elastic.easeOut.config (1, .04)});
+
+var audioplay = document.createElement('audio');
+        audioplay.setAttribute('src', 'boing.wav');
+
+TweenMax.to(".woids",5,{x:200,
+    onStart:function(){audioplay.play()},
+    onComplete:function(){audioplay.pause()}﻿
+})
